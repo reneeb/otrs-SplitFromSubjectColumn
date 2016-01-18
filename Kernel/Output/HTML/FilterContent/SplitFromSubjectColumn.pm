18b07ae97617f0ff0370fa7841a94b470a7023d0
reneeb <reb@perl-services.de>
2016-01-18 14:31:39 +0100

    changes for OTRS5

--- Kernel/Output/HTML/FilterContent/SplitFromSubjectColumn.pm
+++ Kernel/Output/HTML/FilterContent/SplitFromSubjectColumn.pm
@@ -1,20 +1,18 @@
 # --
-# Kernel/Output/HTML/OutputFilterSplitFromSubjectColumn.pm
-# Copyright (C) 2014 Perl-Services.de, http://www.perl-services.de/
+# Kernel/Output/HTML/FilterContent/SplitFromSubjectColumn.pm
+# Copyright (C) 2014 - 2016 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::OutputFilterSplitFromSubjectColumn;
+package Kernel::Output::HTML::FilterContent::SplitFromSubjectColumn;
 
 use strict;
 use warnings;
 
 use List::Util qw(first);
-
-our $VERSION = '0.02';
 
 our @ObjectDependencies = qw(
     Kernel::System::Web::Request
